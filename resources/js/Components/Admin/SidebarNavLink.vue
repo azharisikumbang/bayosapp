4c6e582ec67cf6f9b446bae7e3d2e867f7d2293a
Azhari <azharisaputrasikumbang@gmail.com>
2022-08-28 22:54:19 +0700

    add sidebar nav item placeholder

---
+++ resources/js/Components/Admin/SidebarNavLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+const props = defineProps(['href', 'icon']);
+</script>
+
+<template>
+    <Link :href="href" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+        <i :class="icon"></i>  
+        <slot />
+    </Link>
+</template>
---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AdminLayout from "@/Layouts/Admin.vue";
+</script>
+
+<template>
+    <AdminLayout>
+        <div>Content here</div>
+    </AdminLayout>
+</template>
